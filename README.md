22b3cf26339264767b616ed1ddb4b98d0ea7f2ef
tjjone98 <tzero1898@gmail.com>
2020-07-11 22:24:30 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Simple Rest-api with Rust, Actix
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+pub mod schema;
+pub mod db_connection;
+pub mod models;
+pub mod api;
+
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+extern crate serde;
+extern crate serde_json;
+#[macro_use] 
+extern crate serde_derive;
+
+extern crate actix;
+extern crate actix_web;
+
+use actix_web::{App, HttpServer, web};
+
+fn main() {
+    let sys = actix::System::new("mystore");
+
+    HttpServer::new(
+    || App::new()
+        .service(
+            web::resource("/products")
+                .route(web::get().to_async(api::products::index))
+                .route(web::post().to_async(api::products::create))
+        )
+        .service(
+            web::resource("/products/{id}")
+                .route(web::get().to_async(api::products::show))
+                .route(web::delete().to_async(api::products::destroy))
+                .route(web::patch().to_async(api::products::update))
+        )
+    )
+    .bind("127.0.0.1:8088").unwrap()
+    .start();
+
+    println!("Started http server: 127.0.0.1:8088");
+    let _ = sys.run();
+}
